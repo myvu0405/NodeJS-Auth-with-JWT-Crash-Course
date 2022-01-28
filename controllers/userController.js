962cc40865506af7d44035ee3512c96f18e325b6
My Vu <ngamy.menses.vu@gmail.com>
2022-01-29 00:51:05 +0100

    add error handling for login/signup functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,13 +5,46 @@ const jwt=require('jsonwebtoken');
 const maxAge = 3*24*60*60; //3 days in seconds
 const secretKey=process.env.SECRET_KEY;
 
+//function to handle errors
+const errHandler = (err) => {
 
-const createJwtToken=(id) => jwt.sign({id}, secretKey, {expiresIn:maxAge})
+    let errors = {username: '', email: '', password: ''};
+    //if inputting incorrect email
+    if(err.message.includes('email')) {
+        errors.email=err.message;
+    }
+    
+    //if inputting incorrect password
+    if(err.message.includes('password')) {
+        errors.password=err.message;
+    }
+
+    //check duplicate error code (for signing up an duplicate email)
+    if (err.code===11000) {
+        errors.email='This email is already registered!';
+        return errors;
+    }
+
+    //validation errors
+    if(err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path]=properties.message;
+        });
+    }
+    return errors;
+}
+
+//function to create jwt token
+const createJwtToken= (id) => jwt.sign({id}, secretKey, {expiresIn:maxAge})
 
 
 const signupFunc = async (req,res) => {
+
+    let signupInfo={username:'',email:'',password:''};
+
     if (req.method=='GET') {
-        res.render('users/signup', {pageTitle: 'Signup form'});
+        
+        res.render('users/signup', {pageTitle: 'Signup form',signupVals:signupInfo, errors: false});
     } else if (req.method=='POST') {
 
         // User.create(req.body)
@@ -31,26 +64,36 @@ const signupFunc = async (req,res) => {
             res.redirect('/');
         }
         catch(err){
-            console.log(err)
+            //console.log('ERROR OCCURS: ',err.errors);
+            const errors=errHandler(err);
+            signupInfo=req.body;
+            res.render('users/signup', {pageTitle: 'Signup form',signupVals:signupInfo, errors});
+            //console.log(errors);
         }
     }
 }
 
 const loginFunc = (req,res)=> {
+    let loginInfos={email:'',password:''};
+
     if (req.method=='GET') {
-        res.render('users/login', {pageTitle: 'Login form'});
+        res.render('users/login', {pageTitle: 'Login form',loginVals:loginInfos, errors:false});
     } else if (req.method=='POST') {
         const {email,password} = req.body;
         User.login(email,password)
             .then(user => {
-                console.log('Found user in DB..Allow user to login.');
+                
                 //create token
                 const token=createJwtToken(user._id);
                 //set token in cookies
                 res.cookie('jwt',token,{httpOnly:true, maxAge:maxAge*1000});
                 res.redirect('/');
             })
-            .catch(error => console.log(error))
+            .catch(error => {
+                loginInfos=req.body;
+                const errors=errHandler(error);
+                res.render('users/login',{pageTitle:'Login form',loginVals:loginInfos, errors});
+            })
     }
 
 }
--- models/user.js
+++ models/user.js
@@ -7,19 +7,20 @@ const {Schema} = mongoose;
 const userSchema = new Schema ({
     username: {
         type: String,
-        required: true,
+        required: [true, 'Please enter your username!'],
+        minlength: [3, 'Minimum username length is 3 characters!']
     },
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter your email address!'],
         lowercase: true,
         unique: true,
-        validate: isEmail
+        validate: [isEmail, 'Email address is not valid!']
     },
     password: {
         type: String,
-        required: true,
-        minlength: 5
+        required: [true, 'Please enter your password!'],
+        minlength: [6, 'Minimum password length is 6 characters!']
         //validate: isHash
     }
 }, {timestamps: true});
@@ -35,15 +36,21 @@ userSchema.statics.login = async function (email,password) {
     if (email=='') throw new Error('Please input your email address!');
     if (password=='') throw new Error('Please input your password!');
 
-    try {//search user email address in db
-        const user= await this.findOne({email:email});
+    //search user email address in db
+    const user= await this.findOne({email:email});
+    if(user) {
+        
         //compare passwords
-        const auth=await bcrypt.compare(password,user.password);
-        if(auth) return user;
-        throw new Error('Incorrect password!');
+        const authUser= await bcrypt.compare(password,user.password);
+        if(authUser){
+                
+                return user;
+        }
+        else throw new Error('Incorrect password! Please enter your password again!');
+            
 
     }
-    catch(err) {
+    else {
         throw new Error('Incorrect email address!');
     }
 
